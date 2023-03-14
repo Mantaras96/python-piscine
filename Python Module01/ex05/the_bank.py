3573c6e7714a98435b5ff15907db627f62a40fe1
Albert Mantaras <34039293+Mantaras96@users.noreply.github.com>
2023-03-14 23:09:28 +0000

    Finish bank

--- Python Module01/ex05/the_bank.py
+++ Python Module01/ex05/the_bank.py
@@ -31,12 +31,8 @@ class Bank:
         """
         if not isinstance(new_account, Account):
             raise ValueError("Account must be an Account object")
-        # self.accounts.append(account)
         if any(account.name == new_account.name for account in self.accounts):
             print("Invalid account type or name already exists in the bank.")
-            return False
-        if (isAccountCorrupted(new_account)):
-            print("Invalid corrupted account.")
             return False
         self.accounts.append(new_account)
         
@@ -70,29 +66,75 @@ class Bank:
         if (not isinstance(origin, str) or not isinstance(dest, str)):
             print("Origin/Destini must be a str.")
             return False
+        accountOrigin = self.get_account(origin)
+        accountDest = self.get_account(dest)
+
+        if (accountOrigin.value >= amount):
+            accountOrigin.value -= amount
+            accountDest.value += amount
+            return True
+        else:
+            print("Origin account has not enough money.")
+            return False
         
+    def  fix_account(self, name):
+        """ fix account associated to name if corrupted
+        @name: str(name) of the account
+        @return True if success, False if an error occured
+        """
+        isValid = True
 
+        if not isinstance(name, str):
+            print("Name must be a str.")
+            return False
+        account = self.get_account(name)
+        if (account == None):
+            print("Account not found.")
+            return False
+        keys = list(account.__dict__.keys())
+        if 'name' not in keys:
+            account.name = 'Restored account'
+        if 'id' not in keys:
+            account.id = Account.ID_COUNT
+            Account.ID_COUNT += 1
+        if 'value' not in keys:
+            account.value = 0
+        if (not any(key.startswith("zip") for key in keys)):
+            account.zip = '000-000'
+        if (not any(key.startswith("addr") for key in keys)):
+            account.addr = 'Restored address'
+        keys_to_remove = [key for key in account.__dict__.keys() if key.startswith("b")]
+        for key in keys_to_remove:
+            account.__dict__.pop(key)
+        if (len(account.__dict__) % 2 == 0):
+            for key in keys:
+                if key == 'name' or key == 'id' or key == 'value':
+                    pass
+                elif key.startswith('zip') or key.startswith('addr'):
+                    pass
+                else:
+                    account.__dict__.pop(key)
+                    break
+        if (is_account_corrupted(account)):
+            print("Account is corrupted.")
+            return False
+        print("Account fixed.")
+        return True
 
-def isAccountCorrupted(account):
+def is_account_corrupted(account):
     """ Check if the account is corrupted
     @account: Account() account to check
     @return True if corrupted, False if not
     """
-    corrupted = False
-    
-    print(len(account.__dict__) % 2 == 1 )
+    print(len(account.__dict__) % 2 == 0 )
     print(any(key.startswith("b") for key in account.__dict__.keys()))
-    print(any(key.startswith("zip") for key in account.__dict__.keys()))
-    print(any(key.startswith("addr") for key in account.__dict__.keys()))
+    print(not any(key.startswith("zip") for key in account.__dict__.keys()))
+    print(not any(key.startswith("addr") for key in account.__dict__.keys()))
     print('name' not in list(account.__dict__.keys()))
-    print(not isinstance(account.name, str))
     print('id' not in list(account.__dict__.keys()))
-    print(not isinstance(account.id, int))
     print('value' not in list(account.__dict__.keys()))
-    print(not isinstance(account.value, float or int))
-    print(account.__dict__.keys())
 
-    if (len(account.__dict__) % 2 == 1 or
+    if (len(account.__dict__) % 2 == 0 or
             any(key.startswith("b") for key in account.__dict__.keys()) or
             not any(key.startswith("zip") for key in account.__dict__.keys()) or
             not any(key.startswith("addr") for key in account.__dict__.keys()) or
@@ -111,19 +153,23 @@ def	main( ):
     bank.add(Account(
         'Smith Jane',
         addr='1234 Main Street',
-        zip='911-745',
         value=1000.0,
         ref='58ba2b9954cd278eda8a84147ca73c87',
     ))
-    bank.add(Account(
-        'William John',
-        zip='100-064',
-        value=6460.0,
-        ref='58ba2b9954cd278eda8a84147ca73c87',
-        other='This is the vice president of the corporation'
-    ))
+    # bank.add(Account(
+    #     'William John',
+    #     addr='1234 Main Street',
+    #     zip='911-745',
+    #     value=3000.0,
+    #     ref='58ba2b9954cd278eda8a84147ca73c87',
+    # ))
     
     print(bank.get_account("Smith Jane").id);
+    print(bank.fix_account("Smith Jane"));
+    print(bank.get_account("Smith Jane").zip);
+    # print(bank.get_account("William John").value);
+    # bank.transfer("Smith Jane", "William John", 500.0)
+    # print(bank.get_account("William John").value);
 
 if __name__ == "__main__":
     main()
