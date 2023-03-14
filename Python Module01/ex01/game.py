cca4db62aa2dcc54259e8bc8d81f16e9e92fbd76
Albert Mantaras <34039293+Mantaras96@users.noreply.github.com>
2023-03-14 16:06:48 +0000

    Game add main and the_bank add main and fix soem functions

--- Python Module01/ex01/game.py
+++ Python Module01/ex01/game.py
@@ -1,17 +1,38 @@
 
 class GotCharacter:
+    """ A class representing the "Game of Thrones" character. """
     def __init__(self, first_name, is_alive = True):
         self.first_name = first_name
         self.is_alive = is_alive
 
 class Lannister(GotCharacter):
+    """ A class representing the Lannister family. Or when bad things happen to good people. """
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Lannister"
         self.house_words = "Hear Me Roar"
-    
+
     def print_house_words(self):
+        """ Print the house words of the Lannister family """
         print(self.house_words)
 
     def die(self):
+        """ Kill the character """
         self.is_alive = False
+
+    def	__str__(self) -> str:
+        txt = "{} with house {} and house words '{}' is alive? {}".format(
+            self.first_name,
+            self.family_name,
+            self.house_words,
+            self.is_alive)
+        return txt
+
+def	main( ):
+    """ Main function """
+    a = Lannister("Peter", True)
+    a.print_house_words()
+    print(a)
+    
+if __name__ == "__main__":
+    main()
--- Python Module01/ex05/the_bank.py
+++ Python Module01/ex05/the_bank.py
@@ -1,4 +1,24 @@
+
 # in the_bank.py
+class Account(object):
+    ID_COUNT = 1
+    
+    def __init__(self, name, **kwargs):
+        self.__dict__.update(kwargs)
+        self.id = self.ID_COUNT
+        Account.ID_COUNT += 1
+        self.name = name
+        if not hasattr(self, 'value'):
+            self.value = 0
+
+        if self.value < 0:
+            raise AttributeError("Attribute value cannot be negative.")
+        if not isinstance(self.name, str):
+            raise AttributeError("Attribute name must be a str object.")
+    
+    def transfer(self, amount):
+        self.value += amount
+
 class Bank:
     """ Bank class"""
     def __init__(self):
@@ -13,7 +33,97 @@ class Bank:
             raise ValueError("Account must be an Account object")
         # self.accounts.append(account)
         if any(account.name == new_account.name for account in self.accounts):
+            print("Invalid account type or name already exists in the bank.")
+            return False
+        if (isAccountCorrupted(new_account)):
+            print("Invalid corrupted account.")
+            return False
+        self.accounts.append(new_account)
+        
+    def get_account(self, name):
+        """Return account object with given name"""
+
+        for account in self.accounts:
+            print(account.name)
+            if account.name == name:
+                return account
+        print(f"Account {name} not found in the bank.")
+        return None
+    
+    def transfer(self, origin, dest, amount):
+        """" Perform the fund transfer
+        @origin: str(name) of the first account
+        @dest: str(name) of the destination account
+        @amount: float(amount) amount to transfer
+        @return True if success, False if an error occured
+        """
+        
+        if (origin == dest):
+            print("Origin and destination accounts must be different.")
+            return False
+        if (amount < 0):
+            print("Amount must be positive.")
+            return False
+        if (not isinstance(amount, float)):
+            print("Amount must be a float.")
+            return False
+        if (not isinstance(origin, str) or not isinstance(dest, str)):
+            print("Origin/Destini must be a str.")
             return False
         
-    
 
+
+def isAccountCorrupted(account):
+    """ Check if the account is corrupted
+    @account: Account() account to check
+    @return True if corrupted, False if not
+    """
+    corrupted = False
+    
+    print(len(account.__dict__) % 2 == 1 )
+    print(any(key.startswith("b") for key in account.__dict__.keys()))
+    print(any(key.startswith("zip") for key in account.__dict__.keys()))
+    print(any(key.startswith("addr") for key in account.__dict__.keys()))
+    print('name' not in list(account.__dict__.keys()))
+    print(not isinstance(account.name, str))
+    print('id' not in list(account.__dict__.keys()))
+    print(not isinstance(account.id, int))
+    print('value' not in list(account.__dict__.keys()))
+    print(not isinstance(account.value, float or int))
+    print(account.__dict__.keys())
+
+    if (len(account.__dict__) % 2 == 1 or
+            any(key.startswith("b") for key in account.__dict__.keys()) or
+            not any(key.startswith("zip") for key in account.__dict__.keys()) or
+            not any(key.startswith("addr") for key in account.__dict__.keys()) or
+            'name' not in account.__dict__.keys() or
+            not isinstance(account.name, str) or
+            'id' not in account.__dict__.keys() or
+            not isinstance(account.id, int) or
+            'value' not in account.__dict__.keys() or
+            not isinstance(account.value, (float, int))):
+        return True
+    return False
+
+def	main( ):
+    """ Main function """
+    bank = Bank()
+    bank.add(Account(
+        'Smith Jane',
+        addr='1234 Main Street',
+        zip='911-745',
+        value=1000.0,
+        ref='58ba2b9954cd278eda8a84147ca73c87',
+    ))
+    bank.add(Account(
+        'William John',
+        zip='100-064',
+        value=6460.0,
+        ref='58ba2b9954cd278eda8a84147ca73c87',
+        other='This is the vice president of the corporation'
+    ))
+    
+    print(bank.get_account("Smith Jane").id);
+
+if __name__ == "__main__":
+    main()
